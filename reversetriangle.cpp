df72888455dbad9b891f101d3fa686724e53b55c
Mishthi Chaurasia <2k23.ece2311300@gmail.com>
2025-10-17 23:06:03 +0530

    Add files via upload

---
+++ reversetriangle.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n=4;
+    // cin >> n;
+
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
